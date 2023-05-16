2712237e0eec957b43fa1da5aa6032acd4af16e1
João Natan <joaonatan67548@gmail.com>
2023-05-16 18:20:14 -0300

    Listagem de usuario atualizada

--- index.php
+++ index.php
@@ -11,53 +11,54 @@
     <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
-    <h1>Lista de usuarios</h1>
-
-    <a href="cadastrar.php" id="link_cadastrar">Cadastrar Novo Usuario</a>
-
-    <table id="tbl_usuarios">
-        <thead>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Situação</th>
-            <th>Nivel de acesso</th>
-            <th>Ações</th>
-        </thead>
-
-        <tbody>
-            <?php 
-                $query_usuarios = 
-                "SELECT usuarios.id AS id_usu, usuarios.nome AS nome_usu, usuarios.email AS email_usu, sits_usuarios.nome AS nome_sit, niveis_acessos.nome AS nome_nivel
-                FROM usuarios
-                LEFT JOIN sits_usuarios ON usuarios.sit_usuario_id = sits_usuarios.id
-                LEFT JOIN niveis_acessos ON usuarios.nivel_acesso_id = niveis_acessos.id
-                ORDER BY usuarios.id ASC";
-
-                $result_usuarios = $conexao->prepare($query_usuarios);
-                $result_usuarios->execute();
+    <main>
+        <h1>Lista de usuarios</h1>
+        <a href="cadastrar.php" id="link_cadastrar">Cadastrar Novo Usuario</a>
+        <table id="tbl_usuarios">
+            <thead>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Situação</th>
+                <th>Nivel de acesso</th>
+                <th>Ações</th>
+            </thead>
+            <tbody>
+                <?php
+                    /*-------------------------------------------- Lista os usuarios ---------------------------------------------*/
+                    $query_usuarios = 
+                    "SELECT usuarios.id AS id_usu, usuarios.nome AS nome_usu, usuarios.email AS email_usu, sits_usuarios.nome AS nome_sit, niveis_acessos.nome AS nome_nivel
+                    FROM usuarios
+                    LEFT JOIN sits_usuarios ON usuarios.sit_usuario_id = sits_usuarios.id
+                    LEFT JOIN niveis_acessos ON usuarios.nivel_acesso_id = niveis_acessos.id
+                    ORDER BY usuarios.id ASC";
+                    
+                    $result_usuarios = $conexao->prepare($query_usuarios);
+                    $result_usuarios->execute();
         
-                while ($usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
-                    extract($usuario);
-                    echo    "<tr>
-                                <td>$id_usu</td>
-                                <td>$nome_usu</td>
-                                <td>$email_usu</td>
-                                <td>$nome_sit</td>
-                                <td>$nome_nivel</td>
-                                <td>
-                                    <a href='deletar.php?id_usuario=$id_usu' id='link_apagar' onclick='return confirmacao()'>Apagar</a>
-                                    <a href='editar.php?id_usuario=$id_usu' id='link_editar'>Editar</a>
-                                </td>
-                            </tr>";
-                }
-            ?>
-            <script>
-                function confirmacao() {
-                    return confirm("Tem certeza de que deseja excluir este usuário?");
-                }
-            </script>
-        </tbody>
-    </table>
+                    while ($usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
+                        extract($usuario);
+
+                        echo    "<tr>
+                                    <td>$id_usu</td>
+                                    <td>$nome_usu</td>
+                                    <td>$email_usu</td>
+                                    <td>$nome_sit</td>
+                                    <td>$nome_nivel</td>
+                                    <td> 
+                                        <a href='deletar.php?id_usuario=$id_usu' id='link_apagar' onclick='return confirmacao()'>Apagar</a>
+                                        <a href='editar.php?id_usuario=$id_usu' id='link_editar'>Editar</a>
+                                    </td>
+                                </tr>";
+                    }
+                    /*------------------------------------------------------------------------------------------------------------*/
+                ?>
+                <script>
+                    function confirmacao() {
+                        return confirm("Tem certeza de que deseja excluir este usuário?");
+                    }
+                </script>
+        </table>
+    </main>
 </body>
 </html>
